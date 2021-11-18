5b351e92c5406dc6ce3be3b8d4276eb8eca540fd
Tio Idem <77030484+TioIdem@users.noreply.github.com>
2021-11-18 13:02:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+//setup do bot
 const Aoijs = require('aoi.js');
 const config = require('./config.js');
 const bot = new Aoijs.Bot(config.Bot);
 const fs = require('fs');
 
+//obter os status, variaveis e callbacks👍
+require('./handler/status')(bot)
+require('./handler/variables')(bot)
+require('./handler/callbacks')(bot)
+
+
+//carregando os comandos👍
+const loader = new Aoijs.LoadCommands(bot)
+loader.load(bot.cmd, "./commands/")
 
