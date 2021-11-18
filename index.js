e767739932814f21798006a8c7d0643df87be975
Tio Idem <77030484+TioIdem@users.noreply.github.com>
2021-11-18 12:43:01 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const Aoijs = require("aoi.js")
+
+const bot = new Aoijs.Bot({
+  token: "token do seu bot",
+  prefix: "aqui é seu prefixo",
+  intents: "all" 
+//ative todas as intents no discord developer portal, caso contrário não irá funcionar👍
+})
