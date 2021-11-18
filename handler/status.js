eb722605debb47c9f9feac8996999a91ac255ae0
Tio Idem <77030484+TioIdem@users.noreply.github.com>
2021-11-18 13:20:08 -0300

    Create status.js

---
+++ handler/status.js
@@ -0,0 +1,9 @@
+module.exports => (bot) {
+  bot.status({
+    text: "coloque o que quiser aqui",
+    type: "PLAYING", //pode colocar PLAYING, STREAMING, WATCHING, LISTENING ou COMPETING👍
+    time: 12 //tempo em segundos, quando passar 12 segundos o status troca pro status que estiver embaixo👇
+})
+    
+    
+ }
